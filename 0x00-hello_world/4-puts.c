a629ff941da917efb586be8d3ec7fe4ad4345981
root <peninangizwenayo@gmail.com>
2022-03-11 11:07:58 -0800

    Task 4 commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- *main - Entry point 
+ *main - entry point 
  *
- *Return :0 always (Success)
+ *Return :0 after printing the function
  */
 int main(void)
 {
-	puts(:\"Programming is like building a multilingual puzzle");
-		return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
+
