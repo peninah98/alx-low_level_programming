2c3c7af9edf211abcbd2b79ceb70cf2002a0c83f
peninah98 <78850411+peninah98@users.noreply.github.com>
2022-03-17 13:15:11 -0700

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * 2-mul.c- multiply
+ *
+ *Description:multiply two numbers
+ *
+ *@a:input
+ *
+ *@b:input
+ *Return:1 - digit 0 - otherwise
+ *
+ */
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
